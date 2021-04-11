ee31bed02ccfb883dfdd1d5164b3b70081e6fb74
josivantarcio <josivantarcio@gmail.com>
2021-04-11 00:19:57 -0300

    Update resolveSudoku.py

--- SoluçãoSudoku/resolveSudoku.py
+++ SoluçãoSudoku/resolveSudoku.py
@@ -13,57 +13,48 @@ for l in range(9):
                         subLista['A'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 3 and c <= 5:
                         subLista['B'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 6:
                         subLista['C'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                 if l > 2 and l <= 5:
                     if c >= 0 and c <= 2:
                         subLista['D'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 3 and c <= 5:
                         subLista['E'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 6:
                         subLista['F'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                 if l > 5 and l <= 8:
                     if c >= 0 and c <= 2:
                         subLista['G'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 3 and c <= 5:
                         subLista['H'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
                     if c >= 6:
                         subLista['I'] = n
                         bloco.append(subLista.copy())
                         subLista.clear()
-                        temp.clear()
                         break
 numeros.append(subLista)
 for k, v in enumerate(bloco):
