3c55cd1ce54d9edc4340b70b94cb7df987ad8f2b
josivantarcio <josivantarcio@gmail.com>
2021-04-12 17:51:58 -0300

    incluindo funcao
    Foi colocado função na primeira parte para melhor manutenção do código

--- SoluçãoSudoku/resolveSudoku.py
+++ SoluçãoSudoku/resolveSudoku.py
@@ -1,5 +1,9 @@
+def addbloco(x):
+    bloco.append(x.copy())
+    x.clear()
+    
+
 from random import randint
-celulas = []
 numeros = []
 subLista = {}
 bloco = []
@@ -11,48 +15,39 @@ for l in range(9):
                 if l <= 2:
                     if c >= 0 and c <= 2:
                         subLista['a'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 3 and c <= 5:
                         subLista['b'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 6:
                         subLista['c'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                 if l > 2 and l <= 5:
                     if c >= 0 and c <= 2:
                         subLista['d'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 3 and c <= 5:
                         subLista['e'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 6:
                         subLista['f'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                 if l > 5 and l <= 8:
                     if c >= 0 and c <= 2:
                         subLista['g'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 3 and c <= 5:
                         subLista['h'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
                     if c >= 6:
                         subLista['i'] = n
-                        bloco.append(subLista.copy())
-                        subLista.clear()
+                        addbloco(subLista)
                         break
