586f05d052a6f82d22ee6f0528cc22da7ea5fb2e
Josevan Oliveira <josivantarcio@gmail.com>
2021-04-16 14:31:25 -0300

    Varios Teste
    Fazendo o teste na parte 02.

--- SoluçãoSudoku/resolveSudoku.py
+++ SoluçãoSudoku/resolveSudoku.py
@@ -1,60 +1,54 @@
-def addbloco(x):
-    bloco.append(x.copy())
-    x.clear()
-    
-def addsublista(a, b):
-    temp = [b]
-    temp.append(b)
-    subLista[a].append(temp[:])
-    del(temp)
-
-
 from random import randint
-subLista = []
-bloco = []
+subLista = [[],[],[],[],[],[],[],[],[]]
+bloco = [[],[],[]]
+
+
 for l in range(9):
     for c in range(9):
         n = randint(0, 9)#int(input(f"Digite o valor [{l}][{c}] "))
         if n >= 0 and n < 10:
-            while True:
-                for i in range(1,10):
-                    if l <= 2:
-                        if c >= 0 and c <= 2:
-                            numero = i
-                            addsublista(numero, n)
-                            addbloco(subLista)
-                            break
-                        if c >= 3 and c <= 5:
-                            subLista['b'] = n
-                            addbloco(subLista)
-                            break
-                        if c >= 6:
-                            subLista['c'] = n
-                            addbloco(subLista)
-                            break
-                    if l > 2 and l <= 5:
-                        if c >= 0 and c <= 2:
-                            subLista['d'] = n
-                            addbloco(subLista)
-                            break
-                        if c >= 3 and c <= 5:
-                            subLista['e'] = n
-                            addbloco(subLista)
-                            break
-                        if c >= 6:
-                            subLista['f'] = n
-                            addbloco(subLista)
-                            break
-                    if l > 5 and l <= 8:
-                        if c >= 0 and c <= 2:
-                            subLista['g'] = n
-                            addbloco(subLista)
-                            break
-                        if c >= 3 and c <= 5:
-                            subLista['h'] = n
-                            addbloco(subLista)
-                            break
-                        if c >= 6:
-                            subLista['i'] = n
-                            addbloco(subLista)
-                            break
+            
+            if l <= 2:
+                if c >= 0 and c <= 2:
+                    subLista[0].append(n)
+                if c >= 3 and c <= 5:
+                    subLista[1].append(n)
+                if c >= 6:
+                    subLista[2].append(n)
+            bloco[0].append(subLista[:])
+            if l > 2 and l <= 5:
+                if c >= 0 and c <= 2:
+                    subLista[3].append(n)
+                if c >= 3 and c <= 5:
+                    subLista[4].append(n)
+                if c >= 6:
+                    subLista[5].append(n)
+            bloco[1].append(subLista[:])    
+            if l > 5 and l <= 8:
+                if c >= 0 and c <= 2:
+                    subLista[6].append(n)
+                if c >= 3 and c <= 5:
+                    subLista[7].append(n)
+                if c >= 6:
+                    subLista[8].append(n)
+            bloco[2].append(subLista[:])
+zero = bloco.count(0)
+sub = 0
+#while True:
+#    if 0 not in bloco:
+#        break
+#    else:
+for i in range(1,10):
+    if i not in bloco[0][0][0]:
+        print(f'{i} não está no Bloco')
+        for l in range(9):
+            for c in range(9):
+                print(f'{i} está aqui dentro')
+    else:
+        print(f'{i} está no Bloco')
+            
+        
+    
+
+for i in range(len(subLista)):
+    print(subLista[i])
